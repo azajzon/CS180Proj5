505ca42e71a53612cd5ea71cb5a31c6bcb9e0fe7
azajzon <abzajzon@gmail.com>
2021-11-22 21:39:05 -0800

    created basic client server framework where client sends requests and server receives request and sends back response

---
+++ src/Client/gui.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class gui {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Teacher or Student?");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300, 300);
+        JButton button = new JButton("Login as Teacher");
+        frame.getContentPane().add(button);
+        frame.setVisible(true);
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+}
