bbbcd787da70f96b2b2195f104662e9b88b9b22f
appanzad <89482579+appanzad@users.noreply.github.com>
2021-12-12 18:59:20 -0500

    Update Quiz.java

--- src/Quiz.java
+++ src/Quiz.java
@@ -14,81 +14,81 @@ import java.util.ArrayList;
  * @version 11/14/2021
  */
 
-        public class Quiz implements Serializable {
+public class Quiz implements Serializable {
 
-            // initializes the fields
-            private ArrayList<Question> questions;
-            private String quizName;
-            private double score;
-            private int numQuestions;
+    // initializes the fields
+    private ArrayList<Question> questions;
+    private String quizName;
+    private double score;
+    private int numQuestions;
 
-            //Constructs a newly allocated Server.Quiz object with the field values specified by the input parameters
-            public Quiz(ArrayList<Question> questions, String quizName) {
-                this.questions = questions;
-                this.quizName = quizName;
-                this.score = 0;
-                this.numQuestions = questions.size();
-            }
+    //Constructs a newly allocated Server.Quiz object with the field values specified by the input parameters
+    public Quiz(ArrayList<Question> questions, String quizName) {
+        this.questions = questions;
+        this.quizName = quizName;
+        this.score = 0;
+        this.numQuestions = questions.size();
+    }
 
-            // returns the array list of questions
-            public ArrayList<Question> getQuestions() {
-                return questions;
-            }
+    // returns the array list of questions
+    public ArrayList<Question> getQuestions() {
+        return questions;
+    }
 
-            // returns the name of the quiz
-            public String getQuizName() {
-                return quizName;
-            }
+    // returns the name of the quiz
+    public String getQuizName() {
+        return quizName;
+    }
 
-            //sets the quiz name instance variable to the value given as a parameter
-            public void setQuizName(String quizName) {
-                this.quizName = quizName;
-            }
+    //sets the quiz name instance variable to the value given as a parameter
+    public void setQuizName(String quizName) {
+        this.quizName = quizName;
+    }
 
-            // returns the score of a quiz
-            public double getScore() {
-                return score;
-            }
+    // returns the score of a quiz
+    public double getScore() {
+        return score;
+    }
 
-            public void setQuestion(Question q, int index) {
-                questions.set(index, q);
+    public void setQuestion(Question q, int index) {
+        questions.set(index, q);
 
-            }
-            // sets the score instance variable to the value given as a parameter
-            public void setScore(double score) {
-                this.score = score;
-            }
+    }
+    // sets the score instance variable to the value given as a parameter
+    public void setScore(double score) {
+        this.score = score;
+    }
 
-            // returns the number of questions fo a quiz
-            public int getNumQuestions() {
-                return numQuestions;
-            }
+    // returns the number of questions fo a quiz
+    public int getNumQuestions() {
+        return numQuestions;
+    }
 
-            // sets the num questions instance variable to the value given as a parameter
-            public void setNumQuestions(int numQuestions) {
-                this.numQuestions = numQuestions;
-            }
+    // sets the num questions instance variable to the value given as a parameter
+    public void setNumQuestions(int numQuestions) {
+        this.numQuestions = numQuestions;
+    }
 
-            // calculates the total score of a quiz
-            public double calculateTotalScore() {
-                double total = 0;
-                // goes through all the question and add the point
-                // values of all the questions to a total variable and then returns it
-                for (Question question : questions) total += question.getPointValue();
-                return total;
-            }
+    // calculates the total score of a quiz
+    public double calculateTotalScore() {
+        double total = 0;
+        // goes through all the question and add the point
+        // values of all the questions to a total variable and then returns it
+        for (Question question : questions) total += question.getPointValue();
+        return total;
+    }
 
-            // adds a question to the array list of question
-            public void addQuestion(Question q) {
-                questions.add(q);
-                numQuestions += 1;
-            }
+    // adds a question to the array list of question
+    public void addQuestion(Question q) {
+        questions.add(q);
+        numQuestions += 1;
+    }
 
-            // deletes a question from the array list of questions
-            public void deleteQuestion(Question q) {
-                questions.remove(q);
-                numQuestions -= 1;
-            }
+    // deletes a question from the array list of questions
+    public void deleteQuestion(Question q) {
+        questions.remove(q);
+        numQuestions -= 1;
+    }
 
 
-        }
+}
